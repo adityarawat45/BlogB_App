51cadb49c0342f02d915f5496a10b132b8e699eb
Aditya Rawat <rawataditya193@gmail.com>
2025-07-03 01:06:35 +0530

    changed colors and some other stuff in navbar

--- frontend/src/components/SecondBar.tsx
+++ frontend/src/components/SecondBar.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 export const SecondBar = () => {
-    return <div className="sticky top-0 z-50 bg-stone-300 border-b px-4 py-2 md:px-8 md:py-5 flex justify-between items-center">
+    return <div className="sticky top-0 z-50 bg-stone-300 backdrop-blur-xl border-b-stone-400 border-b-2 px-4 py-2 md:px-8 md:py-5 flex justify-between items-center">
     <div className="text-2xl md:text-4xl bg-gradient-to-tr bg-clip-text text-transparent from-stone-700 to-amber-900 font-black cursor-pointer text-slate-900">
         Blog Nest
     </div>
@@ -10,7 +10,7 @@ export const SecondBar = () => {
         <div className="invisible md:visible text-amber-900 font-extrabold text-sm md:text-lg mx-1 md:mx-2">Sign Up</div>
         </Link>
         <Link to={`signin`}>
-        <div className="border  bg-gradient-to-r text-gray-100 font-extrabold text-sm md:text-lg from-stone-700 to-amber-900 rounded-md px-3 py-1.5 ">
+        <div className="border bg-gradient-to-r text-gray-100 font-extrabold text-sm md:text-lg from-stone-700 to-amber-900 rounded-md px-3 py-1.5 ">
             Log In
         </div>
         </Link>
