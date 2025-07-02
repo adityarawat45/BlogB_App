319f1df5470d529b1497d0931eef6286ed0e313e
Aditya Rawat <rawataditya193@gmail.com>
2025-07-03 00:56:07 +0530

    SOme navbar changes

--- frontend/src/components/SecondBar.tsx
+++ frontend/src/components/SecondBar.tsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom"
 
 export const SecondBar = () => {
-    return <div className="sticky top-0 z-50 bg-stone-300 border-b flex py-1 md:py-2 justify-between items-center px-2 md:px-8">
-    <div className="text-lg md:text-xl font-extrabold cursor-pointer text-slate-900">
+    return <div className="sticky top-0 z-50 bg-stone-300 border-b px-4 py-2 md:px-8 md:py-5 flex justify-between items-center">
+    <div className="text-2xl md:text-4xl bg-gradient-to-tr bg-clip-text text-transparent from-stone-700 to-amber-900 font-black cursor-pointer text-slate-900">
         Blog Nest
     </div>
     <div className="flex flex-row gap-2 justify-center items-center">
         <Link to={`signup`}>
-        <div className="invisible md:visible text-slate-900 font-extrabold text-sm md:text-lg mx-1 md:mx-2">Sign Up</div>
+        <div className="invisible md:visible text-amber-900 font-extrabold text-sm md:text-lg mx-1 md:mx-2">Sign Up</div>
         </Link>
         <Link to={`signin`}>
-        <div className="border  bg-gradient-to-r text-gray-100 font-extrabold text-xs md:text-lg from-slate-700 to-stone-900 rounded-md px-2 py-1 ">Log In</div>
+        <div className="border  bg-gradient-to-r text-gray-100 font-extrabold text-sm md:text-lg from-stone-700 to-amber-900 rounded-md px-3 py-1.5 ">
+            Log In
+        </div>
         </Link>
-        
     </div>
 </div>
 }
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -5,32 +5,35 @@ import { Footer } from "../components/Footer";
 
 const Landing = () => {
   return (
-    <div>
+    // <div className="">
+    //   <SecondBar></SecondBar>
+    //   <div className="md:grid md:grid-cols-2 justify-center items-center">
+    //     <div className="lg:h-screen md:bg-stone-200">
+    //       <div className="flex flex-col rounded-lg mx-2 my-5 bg-stone-200 py-2 md:py-7 px-2 md:px-5">
+    //       <div className="text-5xl md:text-6xl md:mr-20 text-wrap text-slate-800 text-left font-extrabold">Blog Nest</div>
+    //       <div className="text-4xl md:text-5xl md:mr-20 text-left font-extrabold text-slate-800">Explore the World of Blogging"</div>
+    //       <div className="text-slate-900 font-medium md:mr-20  text-left text-md mt-2 md:mt-5">Welcome to our blogging hub, where ideas flourish and perspectives thrive. Join us on a journey of discovery, where every word sparks a new conversation and every post invites you to explore deeper.</div>
+    //       </div>
+    //       <div className="flex flex-row gap-5 px-3 md:px-0 lg:px-0 md:ml-6 justify-center lg:justify-start items-center">
+    //       <Link to={`signin`}>
+    //     <div className="border bg-gradient-to-r text-gray-100 font-extrabold text-xs md:text-lg from-slate-700 to-stone-900 rounded-md px-2 py-1 ">Sign In</div>
+    //     </Link>
+    //     <Link to={`signup`}>
+    //     <div className="border  bg-gradient-to-r text-gray-100 font-extrabold text-xs md:text-lg from-slate-700 to-stone-900 rounded-md px-2 py-1 ">Sign Up</div>
+    //     </Link>
+    //       </div>
+    //     </div>
+    //     <div className="flex md:h-screen md:bg-stone-200 flex-col gap-2 px-2 md:px-7 py-3 md:py-7 justify-between items-center">
+    //       <div className="px-2 md:px-7 py-3 md:py-7 rounded-lg bg-stone-200 flex flex-col justify-between items-center">
+    //       <img  className="w-2/5 h-3/6 my-2 "src={bloggingImage}></img>
+    //       <div className="px-5 font-medium text-center text-slate-800">Explore the latest insights, stories, and updates from our vibrant community of bloggers. Dive into a world of knowledge, inspiration, and creativity right at your fingertips. Uncover diverse perspectives and deep insights, where every click opens a new chapter of inspiration and discovery.</div>
+    //       </div>
+    //     </div>
+    //     <Footer></Footer>
+    //   </div>
+    // </div>
+    <div className="md:flex-col lg:grid-cols-2">
       <SecondBar></SecondBar>
-      <div className="md:grid md:grid-cols-2 ">
-        <div className="lg:h-screen md:bg-stone-200">
-          <div className="flex flex-col rounded-lg mx-2 my-5 bg-stone-200 py-2 md:py-7 px-2 md:px-5">
-          <div className="text-5xl md:text-6xl md:mr-20 text-wrap text-slate-800 text-left font-extrabold">Blog Nest</div>
-          <div className="text-4xl md:text-5xl md:mr-20 text-left font-extrabold text-slate-800">Explore the World of Blogging"</div>
-          <div className="text-slate-900 font-medium md:mr-20  text-left text-md mt-2 md:mt-5">Welcome to our blogging hub, where ideas flourish and perspectives thrive. Join us on a journey of discovery, where every word sparks a new conversation and every post invites you to explore deeper.</div>
-          </div>
-          <div className="flex flex-row gap-5 px-3 md:px-0 lg:px-0 md:ml-6 justify-center lg:justify-start items-center">
-          <Link to={`signin`}>
-        <div className="border bg-gradient-to-r text-gray-100 font-extrabold text-xs md:text-lg from-slate-700 to-stone-900 rounded-md px-2 py-1 ">Sign In</div>
-        </Link>
-        <Link to={`signup`}>
-        <div className="border  bg-gradient-to-r text-gray-100 font-extrabold text-xs md:text-lg from-slate-700 to-stone-900 rounded-md px-2 py-1 ">Sign Up</div>
-        </Link>
-          </div>
-        </div>
-        <div className="flex md:h-screen md:bg-stone-200 flex-col gap-2 px-2 md:px-7 py-3 md:py-7 justify-between items-center">
-          <div className="px-2 md:px-7 py-3 md:py-7 rounded-lg bg-stone-200 flex flex-col justify-between items-center">
-          <img  className="w-2/5 h-3/6 my-2 "src={bloggingImage}></img>
-          <div className="px-5 font-medium text-center text-slate-800">Explore the latest insights, stories, and updates from our vibrant community of bloggers. Dive into a world of knowledge, inspiration, and creativity right at your fingertips. Uncover diverse perspectives and deep insights, where every click opens a new chapter of inspiration and discovery.</div>
-          </div>
-        </div>
-      </div>
-      <Footer></Footer>
     </div>
   )
 }
