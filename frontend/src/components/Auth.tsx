5f21af3c5aba8e044c7e57133d4521fa8da3a153
Aditya Rawat <rawataditya193@gmail.com>
2025-07-07 02:51:34 +0530

    removed unused Link

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent,  useState } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { frontendsignupInput } from "../zod";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
